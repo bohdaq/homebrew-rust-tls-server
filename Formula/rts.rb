ae6a2b1d1fb5eab92b0b097644cc92fe96ffe601
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-28 15:47:20 +0200

    8.0.6

--- Formula/rts.rb
+++ Formula/rts.rb
@@ -1,8 +1,8 @@
 class Rts < Formula
   desc "rust-tls-server (rts) is a web server capable of serving static content over https."
   homepage "https://github.com/bohdaq/rust-tls-server/"
-  url "https://github.com/bohdaq/rust-tls-server/archive/refs/tags/8.0.0.tar.gz"
-  sha256 "694db013205084f445d3aa08397eb11d5da40a8427577a689622ac978a406dc0"
+  url "https://github.com/bohdaq/rust-tls-server/archive/refs/tags/8.0.6.tar.gz"
+  sha256 "14e1a6de1c3fc43dd6683107e98590a6df18333181dc272454dd9ca0404317c9"
   license "MIT"
 
   depends_on "rust" => :build
